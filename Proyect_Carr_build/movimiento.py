d67bd92b4a1f52de8ed4a5483332ab1cacf40930
Arevalinchy <75390468+Arevalinchy@users.noreply.github.com>
2023-10-26 13:14:07 -0500

    movimiento.py

--- Proyect_Carr_build/movimiento.py
+++ Proyect_Carr_build/movimiento.py
@@ -7,71 +7,68 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
-# # This program requires LEGO EV3 MicroPython v2.0 or higher.
-# # Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# # Create your objects here.
-# ev3 = EV3Brick()
-# motorEV3 = Motor(Port.A) # rueda derecha
-# motorEV3_2 = Motor(Port.D) # rueda izquierda
-
-
-# # Write your program here.
-# wait(1000)
-# a = ev3.buttons.pressed()
-# while len(a) == 0:
-#     motorEV3_2.run(-500)
-#     motorEV3.run(-500)
-    
-#     a = ev3.buttons.pressed()
-
-# motorEV3_2.hold()
-# motorEV3.hold()
-
-#!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port
-from pybricks.tools import wait
-
 # Create your objects here.
 ev3 = EV3Brick()
 motorEV3 = Motor(Port.A)  # Rueda derecha
 motorEV3_2 = Motor(Port.D)  # Rueda izquierda
 
-# Set the speed of the motors (adjust as needed)
-motor_speed = -500
-motor_speed2 = -280
+robot = DriveBase(motorEV3_2,motorEV3,56,220)
+speed = 200 # velocidad mm/s equivalente 20 cm/s
+distancia = 500 # Una distancia inicial de 50 cm
+angulo = 90 #Angulo en grados
+robot.settings(speed)
 
-# Define the duration of movement in milliseconds (5 seconds in this example)
-duration = 12200
+#funcion que lleva el robot hacia adelante, ajusta la velocidad, mueve y luego se detiene.
+def adelante(speed,distancia):
+    robot.settings(speed)
+    robot.straight(-distancia)   
+    robot.stop()
+#Funcion pensada para subir la rampa, esta contempla el movimiento completo
+def subirRampa(speed,distancia):
+    robot.settings(speed)
+    robot.straight(-distancia)
+    robot.stop()    
 
-# Write your program here.
-motorEV3.run(motor_speed)
-motorEV3_2.run(motor_speed)
-
-# Wait for the specified duration
-wait(duration)
-
-
-# # Stop the motors
-motorEV3.stop()
-motorEV3_2.stop()
-
-motorEV3_2.run(motor_speed)
-wait(1500)
-motorEV3_2.stop()
+#Simulamos la subida de la rampa y el giro de 90 grados
+adelante(speed,distancia)
+subirRampa(speed/2,1420)
+adelante(speed,distancia/2)
+robot.turn(-90)
+robot.stop()
+adelante(speed,distancia)
 
 
-motorEV3.run(motor_speed)
-motorEV3_2.run(motor_speed)
+# # Set the speed of the motors (adjust as needed)
+# motor_speed = -500
+# motor_speed2 = -280
 
-# Wait for the specified duration
-wait(1000)
-motorEV3.stop()
-motorEV3_2.stop()
+# # Define the duration of movement in milliseconds (5 seconds in this example)
+# duration = 12200
+
+# # Write your program here.
+# motorEV3.run(motor_speed)
+# motorEV3_2.run(motor_speed)
+
+# # Wait for the specified duration
+# wait(duration)
+
+
+# # # Stop the motors
+# motorEV3.stop()
+# motorEV3_2.stop()
+
+# motorEV3_2.run(motor_speed)
+# wait(1500)
+# motorEV3_2.stop()
+
+
+# motorEV3.run(motor_speed)
+# motorEV3_2.run(motor_speed)
+
+# # Wait for the specified duration
+# wait(1000)
+# motorEV3.stop()
+# motorEV3_2.stop()
 
 
 
