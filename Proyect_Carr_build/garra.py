ae7ce44b7c7dfe19aac8be3e2f3b119c0e69ba71
angelopez_macc <49574428+Angelopezmacc@users.noreply.github.com>
2023-10-30 15:58:44 -0500

    Update garra.py

--- Proyect_Carr_build/garra.py
+++ Proyect_Carr_build/garra.py
@@ -11,32 +11,53 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
+""""
+NO MODIFICAR NINGUNO DE LOS VALORES DE VELOCIDAD
+"""
+
 
 # Create your objects here.
 motorEV3_3 = Motor(Port.B) #elevación garra
 motorEV3_4 = Motor(Port.C) #presión garra
 
 # Función para subir la garra
-def subir_garra(velocidad, tiempo):
+""""
+NO MODIFICAR NINGUNO DE LOS VALORES DE VELOCIDAD
+"""
+def subir_garra():
+    velocidad = 750
+    tiempo = 2500
     motorEV3_3.run(-velocidad)
     wait(tiempo)
     motorEV3_3.hold()
 
 # Función para bajar la garra
-def bajar_garra(velocidad, tiempo):
-    motorEV3_3.run(-velocidad)
+""""
+NO MODIFICAR NINGUNO DE LOS VALORES DE VELOCIDAD
+"""
+def bajar_garra():
+    velocidad = 100
+    tiempo = 1000
+    motorEV3_3.run(velocidad)
     wait(tiempo)
     motorEV3_3.hold()
 
 # Función para apretar la garra
-def apretar_garra(velocidad, tiempo):
-    motorEV3_4.run(velocidad)
+def apretar_garra():
+    velocidad = 500
+    tiempo = 2000
+    motorEV3_4.run(-velocidad)
     wait(tiempo)
     motorEV3_4.hold()
 
 # Función para abrir la garra
-def abrir_garra(velocidad, tiempo):
-    motorEV3_4.run(-velocidad)
+""""
+NO MODIFICAR NINGUNO DE LOS VALORES DE VELOCIDAD
+"""
+def abrir_garra():
+    velocidad = 500
+    tiempo = 1551
+    motorEV3_4.run(velocidad)
     wait(tiempo)
     motorEV3_4.hold()
 
@@ -49,11 +70,16 @@ def reiniciar_garra():
     motorEV3_4.hold()
 
 
-bajar_garra(100,1000)
-apretar_garra(1000,1000)
-subir_garra(100,1000)
-wait(2000)
-bajar_garra(100,1000)
-#abrir_garra(1000,1000)
-subir_garra(100,1000)
-reiniciar_garra()
+# bajar_garra(500,1000)
+# apretar_garra(700,1000)
+# subir_garra(400,1000)
+# wait(2000)
+# bajar_garra(100,1000)
+# #abrir_garra(1000,1000)
+# subir_garra(100,1000)
+# reiniciar_garra()
+
+# abrir_garra()
+bajar_garra()
+apretar_garra()
+subir_garra()
