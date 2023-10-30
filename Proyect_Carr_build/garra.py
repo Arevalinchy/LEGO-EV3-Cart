f04a62c89784e33fe8ca9ccdae0bf2584c2df257
angelopez_macc <49574428+Angelopezmacc@users.noreply.github.com>
2023-10-30 11:50:38 -0500

    garra.py

--- Proyect_Carr_build/garra.py
+++ Proyect_Carr_build/garra.py
@@ -13,26 +13,47 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
 # Create your objects here.
-ev3 = EV3Brick()
 motorEV3_3 = Motor(Port.B) #elevación garra
 motorEV3_4 = Motor(Port.C) #presión garra
 
-
-
-for i in range(2):
-
-    motorEV3_4.run(1000)  
-    wait(1000)
-    motorEV3_4.hold()
-
-    motorEV3_3.run(200)  
-    wait(500)
+# Función para subir la garra
+def subir_garra(velocidad, tiempo):
+    motorEV3_3.run(-velocidad)
+    wait(tiempo)
     motorEV3_3.hold()
 
-    motorEV3_4.run(-1000)  
-    wait(1000)
+# Función para bajar la garra
+def bajar_garra(velocidad, tiempo):
+    motorEV3_3.run(-velocidad)
+    wait(tiempo)
+    motorEV3_3.hold()
+
+# Función para apretar la garra
+def apretar_garra(velocidad, tiempo):
+    motorEV3_4.run(velocidad)
+    wait(tiempo)
     motorEV3_4.hold()
 
-    motorEV3_3.run(-200)  
-    wait(500)
+# Función para abrir la garra
+def abrir_garra(velocidad, tiempo):
+    motorEV3_4.run(-velocidad)
+    wait(tiempo)
+    motorEV3_4.hold()
+
+# Función para reiniciar la garra a una posición específica
+def reiniciar_garra():
+    motorEV3_3.reset_angle(0)  # Mover motorEV3_3 a la posición inicial
+    motorEV3_4.reset_angle(0)  # Mover motorEV3_4 a la posición inicial
+    wait(1000)
     motorEV3_3.hold()
+    motorEV3_4.hold()
+
+
+bajar_garra(100,1000)
+apretar_garra(1000,1000)
+subir_garra(100,1000)
+wait(2000)
+bajar_garra(100,1000)
+#abrir_garra(1000,1000)
+subir_garra(100,1000)
+reiniciar_garra()
